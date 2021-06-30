5704bc716f6c3de768cf34b189b8321af175a8f4
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-06-30 10:16:09 +0900

    Initial commit

---
+++ app/src/main/java/com/junglestudio/whatdidilearntoday/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.junglestudio.whatdidilearntoday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
